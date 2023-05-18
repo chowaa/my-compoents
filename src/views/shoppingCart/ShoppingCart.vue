7fbe25594aa47eef085ac35e6f80882be2043e55
chowaa <17671841751@163.com>
2023-05-18 17:28:23 +0800

    add购物车效果

--- src/views/shoppingCart/ShoppingCart.vue
+++ src/views/shoppingCart/ShoppingCart.vue
@@ -1,182 +1,191 @@
 <script lang="ts" setup>
-  import { reactive } from 'vue';
+import { reactive } from 'vue';
 
-  // 商品数据
-  const productData = [
-    { name: '鼠标', price: 100 },
-    { name: '键盘', price: 200 },
-    { name: '耳机', price: 300 },
-    { name: '显示屏', price: 400 }
-  ];
+// 商品数据
+const productData = [
+  { name: '鼠标', price: 100, describe: '这是一个鼠标', },
+  { name: '键盘', price: 200, describe: '这是一个键盘', },
+  { name: '耳机', price: 300, describe: '这是一个耳机', },
+  { name: '显示屏', price: 400, describe: '这是一个显示屏', }
+];
 
-  // 商品数据类型
- class ProductData {
-   name: string;
-   price: number;
-   constructor(name: string, price: number) {
-     this.name = name;
-     this.price = price;
-   }
+// 商品数据类型
+class ProductData {
+  name: string;
+  price: number;
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
   }
-  // 创建商品类
-  class commodityType {
-    // 属性
-    data:ProductData;
-    choose:number;
-    // 构造函数
-    constructor(data:ProductData) {
-      this.data = data;
-      this.choose = 0;
-    }
-    // 获取总价
-    getTotalPrice() {
-      return this.data.price * this.choose;
-    }
-    // 是否选中了此件商品
-    isChoose() {
-      return this.choose > 0;
-    }
-    // 增加商品数量
-    increase() {
-      this.choose++;
-    }
-    // 减少商品数量
-    reduce() {
-      if (this.choose > 0){
-        this.choose--;
-      }
-      return
-    }
+}
+// 创建商品类
+class commodityType {
+  // 属性
+  data: ProductData;
+  choose: number;
+  // 构造函数
+  constructor(data: ProductData) {
+    this.data = data;
+    this.choose = 0;
   }
-
-  // 创建购物车方法类
-  class ShoppingCartMethod {
-    
-    allGoods: commodityType[];// 所有商品
-    
-    upToAmount: number;// 起送金额
-    
-    deliveryFee: number;// 配送费
-
-    // 构造函数
-    constructor() {
-      let allGoods = new Array();
-      productData.map((item) => {
-        allGoods.push(new commodityType(item));
-      });
-      Object.defineProperty(this, 'allGoods', {
-        configurable: false,
-        enumerable: false,
-      })
-      this.allGoods = allGoods;
-      this.upToAmount = 30;
-      this.deliveryFee = 5;
-    }
-
-    // 获取购物车总价
-    getTotalPrice() {
-      let totalPrice = 0;
-      this.allGoods.map((item) => {
-        totalPrice += item.getTotalPrice();
-      });
-      return totalPrice;
-    }
-
-    // 获取购物车商品数量
-    getTotalNumber() {
-      let totalNumber = 0;
-      this.allGoods.map((item) => {
-        totalNumber += item.choose;
-      });
-      return totalNumber;
-    }
-
-    // 增加某件商品数量
-    increase(index:number) {
-      this.allGoods[index].increase();
-    }
-
-    // 减少某件商品数量
-    reduce(index:number) {
-      this.allGoods[index].reduce();
-    }
-    
-    // 判断购物车是否有商品
-    isEmpty() {
-      return this.getTotalNumber() > 0;
-    }
-
-    // 判断购物车是否满足起送金额
-    isUpToAmount() {
-      return this.getTotalPrice() >= this.upToAmount;
-    }
+  // 获取总价
+  getTotalPrice() {
+    return this.data.price * this.choose;
   }
-  const cart = new ShoppingCartMethod();
-  
-  const cartData = reactive(cart) as ShoppingCartMethod;
-
-  const ProductDataRef = reactive(productData);
-  console.log(cartData);
-  
-  const button = (item:number, type:string) => {
-    switch (type) {
-      case 'getTotalPrice':
-        // 购物车总价
-        console.log(cartData.getTotalPrice());
-        break;
-      case 'getTotalNumber':
-        // 购物车商品数量
-        console.log(cartData.getTotalNumber());
-        break;
-      case 'increase':
-        // 增加某件商品数量
-        cartData.increase(item);
-        break;
-      case 'reduce':
-        // 减少某件商品数量
-        cartData.reduce(item);
-        break;
-      case 'isEmpty':
-        // 判断购物车是否有商品
-        console.log(cartData.isEmpty());
-        break;
-      case 'isUpToAmount':
-        // 判断购物车是否满足起送金额
-        console.log(cartData.isUpToAmount());
-        break;
-      case 'isChoose':
-        // 判断某件商品是否选中
-        console.log(cartData.allGoods[item].isChoose());
-        break;
-      // 获取某件商品数量
-      case 'getChoose':
-        console.log(cartData.allGoods[item].choose);
-        break;
-      default:
-        break;
+  // 是否选中了此件商品
+  isChoose() {
+    return this.choose > 0;
+  }
+  // 增加商品数量
+  increase() {
+    this.choose++;
+  }
+  // 减少商品数量
+  reduce() {
+    if (this.choose > 0) {
+      this.choose--;
     }
+    return
+  }
+}
+
+// 创建购物车方法类
+class ShoppingCartMethod {
+
+  allGoods: commodityType[];// 所有商品
+
+  upToAmount: number;// 起送金额
+
+  deliveryFee: number;// 配送费
+
+  // 构造函数
+  constructor() {
+    let allGoods = new Array();
+    productData.map((item) => {
+      allGoods.push(new commodityType(item));
+    });
+    Object.defineProperty(this, 'allGoods', {
+      configurable: false,
+      enumerable: false,
+    })
+    this.allGoods = allGoods;
+    this.upToAmount = 30;
+    this.deliveryFee = 5;
   }
 
-  let someValue: any = "this is a string";
+  // 获取购物车总价
+  getTotalPrice() {
+    let totalPrice = 0;
+    this.allGoods.map((item) => {
+      totalPrice += item.getTotalPrice();
+    });
+    return totalPrice;
+  }
 
-  let strLength: number = (<string>someValue).length;
+  // 获取购物车商品数量
+  getTotalNumber() {
+    let totalNumber = 0;
+    this.allGoods.map((item) => {
+      totalNumber += item.choose;
+    });
+    return totalNumber;
+  }
 
-  console.log(someValue, strLength);
-  
+  // 增加某件商品数量
+  increase(index: number) {
+    this.allGoods[index].increase();
+  }
+
+  // 减少某件商品数量
+  reduce(index: number) {
+    this.allGoods[index].reduce();
+  }
+
+  // 判断购物车是否有商品
+  isEmpty() {
+    return this.getTotalNumber() > 0;
+  }
+
+  // 判断购物车是否满足起送金额
+  isUpToAmount() {
+    return this.getTotalPrice() >= this.upToAmount;
+  }
+}
+const cart = new ShoppingCartMethod();
+
+const cartData = reactive(cart) as ShoppingCartMethod;
+
+const ProductDataRef = reactive(productData);
+console.log(cartData);
+
+const button = (item: number, type: string) => {
+  switch (type) {
+    case 'getTotalPrice':
+      // 购物车总价
+      console.log(cartData.getTotalPrice());
+      break;
+    case 'getTotalNumber':
+      // 购物车商品数量
+      console.log(cartData.getTotalNumber());
+      break;
+    case 'increase':
+      // 增加某件商品数量
+      cartData.increase(item);
+      break;
+    case 'reduce':
+      // 减少某件商品数量
+      cartData.reduce(item);
+      break;
+    case 'isEmpty':
+      // 判断购物车是否有商品
+      console.log(cartData.isEmpty());
+      break;
+    case 'isUpToAmount':
+      // 判断购物车是否满足起送金额
+      console.log(cartData.isUpToAmount());
+      break;
+    case 'isChoose':
+      // 判断某件商品是否选中
+      console.log(cartData.allGoods[item].isChoose());
+      break;
+    // 获取某件商品数量
+    case 'getChoose':
+      console.log(cartData.allGoods[item].choose);
+      break;
+    default:
+      break;
+  }
+}
+
+let someValue: any = "this is a string";
+
+let strLength: number = (<string>someValue).length;
+
+console.log(someValue, strLength);
+
 
 </script>
 
 <template>
   <div class="shopping-cart">
     <h1>Shopping Cart</h1>
-    <div v-for="(item, index) in ProductDataRef" :key="index">
-      <div>
-        <span>商品名：{{ item.name }}&nbsp;&nbsp;&nbsp;</span>
-        <span>价格：￥{{ item.price }}&nbsp;&nbsp;&nbsp;</span>
-        <span>数量：{{ cartData.allGoods[index].choose ? cartData.allGoods[index].choose : 0 }}</span>
+    <div class="commodity-item" v-for="(item, index) in ProductDataRef" :key="index">
+      <div class="commodity-info title">
+        <span class="name">{{ item.name }}&nbsp;&nbsp;&nbsp;</span>
+        <span class="describe">{{ item.describe }}&nbsp;&nbsp;&nbsp;</span>
       </div>
-      <el-button @click="button(index, 'increase')">+</el-button>
-      <el-button @click="button(index, 'reduce')">-</el-button>
+      <div class="prise-button">
+
+        <div class="price">
+          <span>价格：￥{{ item.price }}&nbsp;&nbsp;&nbsp;</span>
+        </div>
+
+        <div class="commodity-button">
+          <el-button v-if="cartData.allGoods[index].choose > 0" @click="button(index, 'reduce')">-</el-button>
+          <span>{{ cartData.allGoods[index].choose ? cartData.allGoods[index].choose : 0 }}</span>
+          <el-button @click="button(index, 'increase')">+</el-button>
+        </div>
+      </div>
     </div>
     <p>商品数量：{{ cartData.getTotalNumber() }}</p>
     <p>起送金额：{{ cartData.upToAmount }}</p>
@@ -186,14 +195,55 @@
 </template>
 
 <style lang="scss" scoped>
-.shopping-cart{
+.shopping-cart {
   width: 100%;
   height: 100%;
   padding: 20px;
   box-sizing: border-box;
   text-align: center;
-  div{
-    margin: 10px 0;
+
+  .commodity-item {
+    display: flex;
+    margin: 5px 0;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 250px;
+    
+    .commodity-info {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+    }
+    .title {
+      .name{
+        font-size: 20px;
+        font-weight: bold;
+      }
+      .describe{
+        color: rgb(133, 133, 133);
+      }
+    }
+    .prise-button{
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      align-items: center;
+      .price {
+        // font-size: 20px;
+        color: rgb(242, 85, 54);
+        font-weight: bold;
+      }
+      .commodity-button {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        span {
+          margin: 0 10px;
+        }
+      }
+    }
   }
 }
 </style>
