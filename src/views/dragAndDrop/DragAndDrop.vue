dc7680d5a5b7418ff93d3190188a3cd0c89be876
chowaa <17671841751@163.com>
2023-05-23 14:51:46 +0800

    添加拖拽复制功能

--- src/views/dragAndDrop/DragAndDrop.vue
+++ src/views/dragAndDrop/DragAndDrop.vue
@@ -7,54 +7,87 @@ class DragItem {
   el: Element | null;
   start: number;
   end: number;
+  startEl: Element | null;
+  dragEnterEl: Element | null;
+  timer: any;
   constructor(name: string) {
     let start = 0;
     let end = 0;
     this.start = start;
     this.end = end;
     this.name = name;
+    this.dragEnterEl = null;
+    this.startEl = null;
+    this.timer = null;
     this.el = document.querySelector(`.${name}`);
   }
   // 拖拽开始
-  dragStart( event:DragEvent, item: any, index: number) {
-    if(event.dataTransfer){
+  dragStart(event: DragEvent, index: number, item?: any) {
+    if (index || event || item) { }
+    if (event.dataTransfer) {
       event.dataTransfer.effectAllowed = 'move';
     }
+    const target = event.target as HTMLElement;
+    if(target.classList.contains('clone')) {
+      return;
+    }
+    this.startEl = target
     this.start = index;
-    console.log(item, index)
-    console.log(event,'dragStart');
   }
   // 拖拽中
-  dragOver( event:DragEvent, item: any, index: number ){
+  dragOver(event: DragEvent, index: number, item?: any) {
+    if (index || event || item) { }
     event.preventDefault();
-    console.log(event,'dragOver');
-    console.log(item, index);
+    if (this.timer !== null) {
+      clearTimeout(this.timer);
+    }
+    this.timer = setTimeout(() => {
+      const target = event.target as HTMLElement;
+      this.dragEnterEl = target;
+    }, 100);
+  }
 
-  }
   // 拖拽结束
-  dragEnd( event:DragEvent, item: any, index: number ) {
+  dragEnd(event: DragEvent, index: number, item?: any) {
     // 获取目标元素
-    const target = event.target as HTMLElement;
-    console.log(target, 'target');
-    console.log(event,'dragEnd');
-    console.log(item, index);
+    if (index || event || item) { }
+    // const target = event.target as HTMLElement;
   }
+
+  // 清除样式
+  clearStyle() {
+    document.querySelectorAll('.dragAndDrop__right__content__item').forEach((item) => {
+      item.classList.remove('dragAndDrop__right__content__item--active');
+    })
+  }
+
   // 拖拽进入
-  dragEnter( event:DragEvent, index: number, item?: any ) {
-    if(item||index){}
+  dragEnter(event: DragEvent, index: number, item?: any) {
+    this.clearStyle()
+    if (item || index) { }
     // 获取目标元素
     const target = event.target as HTMLElement;
     if (target.classList.contains('dragAndDrop__left__content__item')) {
+
+    } else if( target.classList.contains('dragAndDrop__right__content__item') ){
+      target.classList.add('dragAndDrop__right__content__item--active');
     }
   }
   // 拖拽放手
-  dragDrop( event:DragEvent, item: any, index: number ) {
-    if(item){}
+  dragDrop(event: DragEvent, index: number, item?: any) {
+    if (item) { }
     const target = event.target as HTMLElement;
     if (target.classList.contains('dragAndDrop__left__content__item')) {
       const moveItem = leftData[this.start];
       leftData.splice(this.start, 1);
-      leftData.splice(index, 0, moveItem); 
+      leftData.splice(index, 0, moveItem);
+    } else if( target.classList.contains('dragAndDrop__right__content__item') ){
+      this.clearStyle()
+      if(this.startEl){
+        const cloned:any = this.startEl.cloneNode(true);
+        cloned.classList.add('clone')
+        target.appendChild(cloned);
+      }
     }
   }
 }
@@ -84,15 +117,13 @@ const leftData = reactive([
             <div class="dragAndDrop__left">
               <!-- <div class="dragAndDrop__left__title">左侧</div> -->
               <div class="dragAndDrop__left__content dragItem">
-                <div class="dragAndDrop__left__content__item dragAndDrop__box" 
-                  v-for="(item, index) in leftData" :key="item.name" 
-                  :draggable="true" 
-                  @dragstart="dragItem.dragStart( $event, item, index)"
-                  @dragenter="dragItem.dragEnter( $event, index )"
-                  @dragend="dragItem.dragEnd( $event,item, index)"
-                  @dragover="dragItem.dragOver( $event, item, index )"
-                  @drop="dragItem.dragDrop( $event, item, index )"
-                  >
+                <div class="dragAndDrop__left__content__item dragAndDrop__box" v-for="(item, index) in leftData"
+                  :key="item.name" :draggable="true" 
+                  @dragstart="dragItem.dragStart($event, index, item)"
+                  @dragenter="dragItem.dragEnter($event, index)" 
+                  @dragend="dragItem.dragEnd($event, index, item)"
+                  @dragover="dragItem.dragOver($event, index, item)" 
+                  @drop="dragItem.dragDrop($event, index, item)">
                   {{ item.name }}
                 </div>
               </div>
@@ -101,8 +132,12 @@ const leftData = reactive([
           <el-col :span="18">
             <div class="dragAndDrop__right">
               <div class="dragAndDrop__right__content">
-                <div class="dragAndDrop__right__content__item dragAndDrop__box" v-for="index in 9" :key="index"
-                @dragenter="dragItem.dragEnter( $event, index )">
+                <div class="dragAndDrop__right__content__item bg dragAndDrop__box" v-for="index in 9" :key="index"
+                  @dragend="dragItem.dragEnd($event, index)"
+                  @dragenter="dragItem.dragEnter($event, index)" 
+                  @drop="dragItem.dragDrop($event, index)"
+                  @dragover="dragItem.dragOver($event, index)"
+                  >
                   <!-- {{ dragItem.name }} -->
                 </div>
               </div>
@@ -115,35 +150,58 @@ const leftData = reactive([
 </template>
 
 <style lang="scss" scoped>
-.dragAndDrop{
-  --width:210px;
-  --height:75px;
+.clone{
+  -webkit-user-drag:none
+}
+.dragAndDrop {
+  --width: 210px;
+  --height: 75px;
   --margin: 5px auto;
+  --dragAndDrop__left__content__item-bg: antiquewhite;
   text-align: center;
-  .dragAndDrop__box{
+
+  .dragAndDrop__box {
     width: var(--width);
     height: var(--height);
     margin: var(--margin);
   }
-  .dragAndDrop__left{
-    .dragAndDrop__left__content{
+
+  .dragAndDrop__left {
+    .dragAndDrop__left__content {
       display: flex;
       flex-direction: column;
       .drag-move {
         transition: transform 0.3s;
       }
-      .dragAndDrop__left__content__item{
-        background-color:antiquewhite;
+
+      .dragAndDrop__left__content__item {
+        background-color: var(--dragAndDrop__left__content__item-bg);
       }
     }
   }
-  .dragAndDrop__right{
+
+  .dragAndDrop__right {
     width: 100%;
-    .dragAndDrop__right__content{
+
+    .dragAndDrop__right__content {
       display: flex;
       flex-wrap: wrap;
-      .dragAndDrop__right__content__item{
+
+      .dragAndDrop__right__content__item {
+        width: calc(var(--width) + 20px) ;
+        height: calc(var(--height) + 30px);
+        display: flex;
+        align-items: center;
+        .dragAndDrop__left__content__item{
+          background-color: var(--dragAndDrop__left__content__item-bg);
+        }
+      }
+      .bg{
         background-color: aquamarine;
+
+      }
+      .dragAndDrop__right__content__item--active {
+        background-color: #e9ebec;
       }
     }
   }
