54604b4c6db81467e4cd3610bf9963d30a5ed5bb
chowaa <17671841751@163.com>
2023-05-31 10:51:46 +0800

    添加注释

--- src/views/dragAndDrop/DragAndDrop.vue
+++ src/views/dragAndDrop/DragAndDrop.vue
@@ -1,100 +1,169 @@
 <script lang="ts" setup>
 import { reactive } from 'vue'
 
-// 拖动启动数据
+// 定义拖动启动数据类
 class DragStartData {
-  public index: number;
-  public item: any;
+  public index: number;// 拖拽开始索引
+  public item: any;// 拖拽开始元素数据
   constructor(index: number, item: any) {
     this.index = index;
     this.item = item;
   }
 }
 
-// 拖拽事件
+// 定义拖拽事件类
 class DragItem {
-  public start: number = 0;
-  public end: number = 0;
-  public startEl: Element | null = null;
-  public dragEnterEl: Element | null = null;
-  public flag: boolean = false;
+  private start: number = 0;// 拖拽开始索引
+  private end: number = 0;// 拖拽结束索引
+  private startEl: Element | null = null;// 拖拽开始元素
+  private dragEnterEl: Element | null = null;// 拖拽进入元素
+  private flag: boolean = false;// 是否拖拽进入元素
 
-  // 拖拽开始
-  public dragStart(event: DragEvent, index: number, item?: any) {
+  // 拖拽开始方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public dragStart(event: DragEvent, index: number, item?: any) :void {
     if (index || event || item) { }
+    // 如果事件对象有 dataTransfer 属性
     if (event.dataTransfer) {
+      // 设置允许的拖拽效果为 "move"
       event.dataTransfer.effectAllowed = 'move';
     }
+    // 获取事件目标元素，并将其类型转换为 HTMLElement
     const target = event.target as HTMLElement;
+    // 将目标元素赋值给 startEl
     this.startEl = target;
+    // 将传入的索引值赋值给 start
     this.start = index;
   }
 
-  // 拖拽中
-  public dragOver(event: DragEvent, index: number, item?: any) {
+  // 拖拽过程中方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public dragOver(event: DragEvent, index: number, item?: any) :void {
     if (index || event || item) { }
+    // 如果 startEl 不为 null，说明拖拽已经开始
     if (this.startEl !== null) {
+      // 阻止默认事件，以允许拖拽操作继续进行
       event.preventDefault();
     }
   }
 
-  // 拖拽进入
-  public dragEnter(event: DragEvent, index: number, item?: any) {
+  // 拖拽进入方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public dragEnter(event: DragEvent, index: number, item?: any) :void {
     if (index || event || item) { }
+    // 将 flag 设置为默认值 false
     this.flag = false;
+    // 将 dragEnterEl 设置为 null
     this.dragEnterEl = null;
+    // 调用 clearStyle 方法清除样式
     this.clearStyle();
+    // 获取事件目标元素，并将其类型转换为 HTMLElement
     const target = event.target as HTMLElement;
+    // 如果 item 等于 'right' 且 startEl 不为 null
     if (item === 'right' && this.startEl !== null) {
+      // 为目标元素添加 'dragAndDrop__right__content__item--active' 类
       target.classList.add('dragAndDrop__right__content__item--active');
+      // 将目标元素赋值给 dragEnterEl
       this.dragEnterEl = target;
+      // 将 flag 设置为 true
       this.flag = true;
     } else {
+      // 将传入的索引值赋值给 end
       this.end = index;
     }
   }
 
-  // 拖拽离开
-  public dragDrop(event: DragEvent, index: number, leftData: any[], item?: any) {
+  // 拖拽离开方法
+  /**
+   * 
+   * @param event 拖拽元素
+   * @param index 拖拽元素索引
+   * @param leftData 原始数据
+   * @param item 拖拽元素数据
+   */
+  public dragDrop(event: DragEvent, index: number, leftData?: any[], item?: any) :void {
     if (index || event || item) { }
-    const moveItem = leftData[this.start];
-    leftData.splice(this.start, 1);
-    leftData.splice(this.end, 0, moveItem);
+    if(leftData){
+      // 获取要移动的元素
+      const moveItem = leftData[this.start];
+      // 从原始位置移除元素
+      leftData.splice(this.start, 1);
+      // 将元素插入到新位置
+      leftData.splice(this.end, 0, moveItem);
+    }
   }
 
-  // 拖拽结束
-  public dragEnd(event: DragEvent, index: number, item?: any) {
+  // 拖拽结束方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public dragEnd(event: DragEvent, index: number, item?: any) :void {
     if (index || event || item) { }
+    // 清除样式
     this.clearStyle();
+    // 如果 flag 为 true，表示拖拽元素进入了目标元素
     if (this.flag === true) {
+      // 清除目标元素的子元素
       this.clearChild(this.dragEnterEl as Element);
+      // 如果开始拖拽的元素和目标元素都存在
       if (this.startEl && this.dragEnterEl) {
+        // 克隆开始拖拽的元素
         const cloned: any = this.startEl.cloneNode(true);
+        // 清除克隆元素的事件
         this.clearEvent(cloned);
+        // 为克隆元素添加 'noDrag' 类
         cloned.classList.add('noDrag')
+        // 将克隆元素添加到目标元素中
         this.dragEnterEl.appendChild(cloned);
       }
     }
+    // 重置拖拽相关数据
     this.reset()
   }
 
-  // 清除事件
-  private clearEvent(el: Element) {
+  // 清除事件方法
+  /**
+   * @param el 需要清除事件的元素
+   * @returns void
+   */
+  private clearEvent(el: Element) :void {
     const cloneEl = el.cloneNode(true);
     if (el.parentNode) {
       el.parentNode.replaceChild(cloneEl, el);
     }
   }
 
-  // 清除子元素
-  private clearChild(el: Element) {
+  // 清除子元素方法
+  /**
+   * @param el 需要清除子元素的元素
+   * @returns void
+   */
+  private clearChild(el: Element) :void {
     while (el.firstChild) {
       el.removeChild(el.firstChild);
     }
   }
   
-  // 重置数据
-  private reset() {
+  // 重置数据方法
+  private reset() :void {
     this.start = 0;
     this.end = 0;
     this.startEl = null;
@@ -102,47 +171,82 @@ class DragItem {
     this.flag = false;
   }
 
-  // 清除样式
-  private clearStyle() {
+  // 清除样式方法
+  private clearStyle() :void {
     document.querySelectorAll('.dragAndDrop__right__content__item').forEach((item) => {
       item.classList.remove('dragAndDrop__right__content__item--active');
     })
   }
 }
 
+// 定义拖拽类
 class DragAndDrop {
-  private leftData: any[];
-  private dragItem: DragItem;
+  private leftData: any[];// 左侧数据
+  private dragItem: DragItem;// 拖拽元素
 
   constructor(leftData: any[]) {
     this.leftData = leftData;
     this.dragItem = new DragItem();
   }
 
-  public handleDragStart(event: DragEvent, index: number, item?: any) {
+  // 处理拖拽开始方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public handleDragStart(event: DragEvent, index: number, item?: any) :void {
     const dragStartData = new DragStartData(index, item);
     this.dragItem.dragStart(event, dragStartData.index, dragStartData.item);
   }
 
-  public handleDragOver(event: DragEvent, index: number, item?: any) {
+  // 处理拖拽过程中方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public handleDragOver(event: DragEvent, index: number, item?: any) :void {
     this.dragItem.dragOver(event, index, item);
   }
 
-  public handleDragEnter(event: DragEvent, index: number, item?: any) {
+  // 处理拖拽进入方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public handleDragEnter(event: DragEvent, index: number, item?: any) :void {
     this.dragItem.dragEnter(event, index, item);
   }
 
-  public handleDragDrop(event: DragEvent, index: number, item?: any) {
+  // 处理拖拽离开方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public handleDragDrop(event: DragEvent, index: number, item?: any) :void {
     this.dragItem.dragDrop(event, index, this.leftData, item);
   }
 
-  public handleDragEnd(event: DragEvent, index: number, item?: any) {
+  // 处理拖拽结束方法
+  /**
+   * @param event 拖拽事件
+   * @param index 拖拽元素索引
+   * @param item 拖拽元素数据
+   * @returns void
+   */
+  public handleDragEnd(event: DragEvent, index: number, item?: any) :void {
     this.dragItem.dragEnd(event, index, item);
   }
 }
 
-
-// 可拖拽数据
+// 定义可拖拽数据
 const leftData = reactive([
   { name: 'CSS' },
   { name: 'JAVASCRIPT' },
@@ -155,6 +259,8 @@ const leftData = reactive([
   { name: 'AXIOS' }
 ])
 const myDgAndDp:DragAndDrop = new DragAndDrop(leftData);
+
+
 </script>
 
 <template>
